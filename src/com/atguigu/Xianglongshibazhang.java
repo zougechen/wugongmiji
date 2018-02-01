67d83067d2f036790e35871a0c21b9f7e3f42c7f
zougechen <1035645446@qq.comgit config --global user.email 1035645446@qq.com>
2018-02-01 10:52:30 +0800

    更新一

--- src/com/atguigu/Xianglongshibazhang.java
+++ src/com/atguigu/Xianglongshibazhang.java
@@ -3,8 +3,7 @@ package com.atguigu;
 public class Xianglongshibazhang {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("犀牛老仙，武功第一");
 	}
 
 }
